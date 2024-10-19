1f22feefadd947d4a479f452bc59d105373a564f
poornagollakoti <chandra.gollakoti@gmail.com>
2024-10-19 12:55:49 +0530

    CORS error fix

--- src/main.ts
+++ src/main.ts
@@ -18,11 +18,24 @@ async function bootstrap() {
    * file to invoke setup and middleware methods/functions instead.
    */
 
+  app.use((req: any, res: any, next: any) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://40.121.150.44');  // Change this to the exact frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    
+    next();
+  });
+  
   await swaggerSetUp(app);
   await globalPipe(app);
   // security middlewares
   await securityMiddleware(app);
-  //await corsMiddleware(app);
+  // await corsMiddleware(app);
   await bodyParserMiddleware(app);
 
   await app.listen(appConfig().port, () =>
